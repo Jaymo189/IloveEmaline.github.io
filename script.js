ff6df7164d354e727bfc55e8cb523a2d802960a9
Jaymo189 <87294270+Jaymo189@users.noreply.github.com>
2024-02-19 16:08:44 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function checkQuiz() {
     const correctAnswer = "Pufferfish";
     const userAnswer = document.getElementById('quizAnswer').value;
     const correctAnswer2 = "Emaline";
-    const userAnswer2 = document.getElementById('quizAnswer').value;
+    const userAnswer2 = document.getElementById('quizAnswer2').value;
     if(userAnswer === correctAnswer && userAnswer2 === correctAnswer2) {
         window.location.href = 'main.html';
     } else {
