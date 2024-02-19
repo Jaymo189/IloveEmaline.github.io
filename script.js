88e2ebb6513ce7f58f8df53735db04e86558df2f
Jaymo189 <87294270+Jaymo189@users.noreply.github.com>
2024-02-19 18:53:35 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    function checkQuiz() {
-        const correctAnswer = "pufferfish";
-        const userAnswer = document.getElementById('quizAnswer').value.toLowerCase();
-        const correctAnswer2 = "emaline";
-        const userAnswer2 = document.getElementById('quizAnswer2').value.toLowerCase();
-
-        if(userAnswer === correctAnswer && userAnswer2 === correctAnswer2) {
-            // Consider using a more reliable navigation method if window.location.href doesn't work
-            window.location.href = 'main.html';
-        } else {
-            // Consider using a more subtle way to show errors, especially for mobile users
-            alert("Noooo little mems that's not right!");
-        }
+function checkQuiz() {
+    const correctAnswer = "pufferfish";
+    const userAnswer = document.getElementById('quizAnswer').value.toLowerCase(); 
+    const correctAnswer2 = "emaline"; 
+    const userAnswer2 = document.getElementById('quizAnswer2').value.toLowerCase(); 
+    
+    if(userAnswer === correctAnswer && userAnswer2 === correctAnswer2) {
+        window.location.href = 'main.html';
+    } else {
+        alert("Noooo little mems thats not right!");
     }
-
-    // Assuming you have a button with id 'submitQuiz' for users to submit their answers
-    document.getElementById('submitQuiz').addEventListener('touchend', checkQuiz);
-});
+}
