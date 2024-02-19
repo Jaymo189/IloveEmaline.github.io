35ffd717f629afd0df58f08749589140b44841d2
Jaymo189 <87294270+Jaymo189@users.noreply.github.com>
2024-02-19 17:30:29 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function checkQuiz() {
-    const correctAnswer = "Pufferfish" || "pufferfish";
-    const userAnswer = document.getElementById('quizAnswer').value;
-    const correctAnswer2 = "Emaline" || "emaline";
-    const userAnswer2 = document.getElementById('quizAnswer2').value;
+    const correctAnswer = "pufferfish"; // Convert correct answer to lowercase
+    const userAnswer = document.getElementById('quizAnswer').value.toLowerCase(); // Convert user input to lowercase
+    const correctAnswer2 = "emaline"; // Convert correct answer to lowercase
+    const userAnswer2 = document.getElementById('quizAnswer2').value.toLowerCase(); // Convert user input to lowercase
+    
     if(userAnswer === correctAnswer && userAnswer2 === correctAnswer2) {
         window.location.href = 'main.html';
     } else {
