98d7647d41160489057ff41f3b29fd950a2e5133
Jaymo189 <87294270+Jaymo189@users.noreply.github.com>
2024-02-19 19:05:20 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function checkQuiz() {
     const correctAnswer = "pufferfish";
-    const userAnswer = document.getElementById('quizAnswer').value.toLowerCase(); 
+    const userAnswer = document.getElementById('quizAnswer').value.toLowerCase().trim(); 
     const correctAnswer2 = "emaline"; 
-    const userAnswer2 = document.getElementById('quizAnswer2').value.toLowerCase(); 
+    const userAnswer2 = document.getElementById('quizAnswer2').value.toLowerCase().trim(); 
     
     if(userAnswer === correctAnswer && userAnswer2 === correctAnswer2) {
         window.location.href = 'main.html';
     } else {
-        alert("Noooo little mems thats not right!");
+        alert("Noooo little mems that's not right!");
     }
 }
